f7292a9f04ee4716f1b314a6a9f1190227ecccf0
AdarshKhare29 <adarshkhare86@gmail.com>
2021-04-18 23:53:05 +0530

    first commit

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 // import Todo from "./Todo"
 
 const TodoList = ({ todoList, handleToggle, handleFilter, deleteTodo, completedTodo }) => {
